e8ec2526d4ec68879ca34c5417cadba4bbce7fcc
brunoslvmnz <83134972+brunoslvmnz@users.noreply.github.com>
2022-07-06 11:59:31 -0300

    Add files via upload
    atualização para captura de dados

--- src/components/sections/contato.js
+++ src/components/sections/contato.js
@@ -1,40 +1,98 @@
 import React, { Component } from 'react';
+import { useState } from "react";
+import * as api from "../../services/Endpoints";
 import '../css/ourstrap.css';
 import '../css/style.css';
 import contatofoto from '../imgs/cttofoto.png'
+
+
+//insere nas variaveis
+const NovoLead = () => {
+    const estadoInicial = {
+        id: null,
+        nome: "",
+        telefone: "",
+        email: "",
+    };
+    const [lead, setLead] = useState(estadoInicial);
+    const [submitted, setSubmitted] = useState(false);
+
+    const trataCampo = (event) => {
+        const { name, value } = event.target;
+        setLead({ ...lead, [name]: value });
+    };
+
+    //captura e envio para o banco
+    const enviarLead = () => {
+        api
+            .create(lead)
+            .then((response) => {
+                setLead({
+                    id: response.data.id,
+                    nome: response.data.nome,
+                    telefone: response.data.telefone,
+                    email: response.data.email,
+                });
+                setSubmitted(true);
+                console.log(response.data);
+            })
+            .catch((e) => {
+                console.log(e);
+            });
+    };
+};
+
 
 class Contato extends Component {
     render() {
         return (
             <section>
-            <div className="container">
-                <div className="row justify-content-center p-5">
-                    <div className="col-md-auto ">
-                        <h2>Vamos conversar?</h2>
-                        <h4>Será um enorme prazer te conhecer!</h4>
+                <div className="container">
+                    <div className="row justify-content-center p-5">
+                        <div className="col-md-auto ">
+                            <h2>Vamos conversar?</h2>
+                            <h4>Será um enorme prazer te conhecer!</h4>
+                        </div>
+                    </div>
+
+                    <div className="d-xl-flex justify-content-center">
+                        <div className="centro">
+                            <img src={contatofoto} alt="" className="quemsoueufoto quemsoueufotopq" />
+                        </div>
+                        <div className="align-self-md-center p-5">
+                            <form>
+                                <label>Nome:</label><br />
+                                <input type="text" className="" size="30"
+                                    id="nome"
+                                    required
+                                    value={lead.nome}
+                                    onChange={trataCampo}
+                                    name="nome" /><br /><br />
+                                <label>Telefone:</label><br />
+                                <input type="text" className="" size="30"
+                                    id="telefone"
+                                    required
+                                    value={lead.telefone}
+                                    onChange={trataCampo}
+                                    name="telefone" /><br /><br />
+                                <label>E-mail:</label><br />
+                                <input className="" size="30"
+                                    id="email"
+                                    required
+                                    value={lead.email}
+                                    onChange={trataCampo}
+                                    name="email"
+                                    type="email" /><br />
+                                <input type="submit" className="botao" value="ENVIAR" onClick={enviarLead} />
+                            </form>
+                        </div>
                     </div>
                 </div>
-    
-                <div className="d-xl-flex justify-content-center">
-                    <div className="centro">
-                        <img src={contatofoto} alt="" className="quemsoueufoto quemsoueufotopq"/>
-                    </div>
-                    <div className="align-self-md-center p-5">
-                        <form>
-                            <label>Nome:</label><br/>
-                            <input type="text" className="" size="30"/><br/><br/>
-                            <label>Telefone:</label><br/>
-                            <input type="text" className="" size="30"/><br/><br/>
-                            <label>E-mail:</label><br/>
-                            <input type="text" className="" size="30"/><br/>
-                            <input type="button" className="botao" value="ENVIAR"/>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </section>
+            </section>
         );
     }
 }
 
 export default Contato;
+
+
